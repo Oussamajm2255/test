54959186ab08d268b9e629bc1babf3534d68cbd6
Oussamajm2255 <167373349+Oussamajm2255@users.noreply.github.com>
2024-09-01 23:15:20 +0100

    Update database.js

--- server/config/database.js
+++ server/config/database.js
@@ -1,16 +1,16 @@
 const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
-  host: 'sql209.infinityfree.com',  // MySQL Host Name
-  user: 'if0_37222858',            // MySQL User Name
-  password: 'SSJmldJnlLV', // MySQL Password (Replace with your actual vPanel password)
-  database: 'if0_37222858_Stoppages_Logs_database', // MySQL DB Name
+  host: 'localhost',
+  user: 'root',
+  password: 'Oussama2255',
+  database: 'new_schema',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 });
 
-connection.getConnection(async (err, conn) => {
+connection.getConnection((err, conn) => {
   if (err) {
     console.error('Error connecting to database: ', err);
     return;
